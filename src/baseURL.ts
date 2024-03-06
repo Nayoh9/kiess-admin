695b7f65d430d04d85972d0cd3c3611cd231a792
Yohann Andre <y.andre90000@gmail.com>
2024-03-06 17:49:39 +0100

    add new base url online

--- src/baseURL.ts
+++ src/baseURL.ts
@@ -1,4 +1,4 @@
-const baseURL:String = "http://localhost:3000"
-// const baseURL:String
+// const baseURL:String = "http://localhost:3000"
+const baseURL:String = "https://site--kiess-backend--s7xgqdjwl4w7.code.run/"
 
 export default baseURL
